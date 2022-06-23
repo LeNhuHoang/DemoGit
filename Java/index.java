7b21653f543dece56a90f003683a75847ef01050
LeNhuHoang <lnhoang1234@gmail.com>
2022-06-24 00:16:18 +0700

    Kiem tra chan le

--- Java/index.java
+++ Java/index.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class App{
 	public static void main(String[] args){
-		System.out.println("Hello word");
+		System.out.println("Hello word.");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhap so.");
+		int n = sc.nextInt();
+		if(n % 2 == 0){
+			System.out.print(n+"La so chan.");
+		}else{
+			System.out.print(n + "La so le.");
+		}
+
 	}
 }
